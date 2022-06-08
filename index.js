61723fbd878e3cc442512505748ec17fc70d98d0
Denis Maina <Denis.maina@student.moringaschool.com>
2022-06-08 14:51:26 +0300

    commit tree 7f782d4d0997bc29141928caf1aef2673e259af0

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+saturdayFun("bathe my dog");
+
+const mondayWork = function(role = "go to the office"){
+    return `This Monday, I will ${role}.`
+}
+mondayWork();
+mondayWork("work from home");
+
+let wrapAdjective = function(style="*") {
+    return function(adjective="special person") {
+      return `You are ${style}${adjective}${style}!`
+    }
+  }
